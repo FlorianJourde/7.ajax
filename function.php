9e9f4cb2f1828a214c7a2e4ed25eab51492008d8
Florian <jourdeflorian@gmail.com>
2021-03-01 15:54:50 +0100

    affichage dossiers

--- function.php
+++ function.php
@@ -1,14 +1,14 @@
-
 <?php
-$dir = "/var/www/html";
 
-// Ouvre un dossier bien connu, et liste tous les fichiers
+$dir = "/var/www/html/7.ajax";
+
 if (is_dir($dir)) {
-    if ($dh = opendir($dir)) {
-        while (($file = readdir($dh)) !== false) {
-            echo "fichier : $file : type : " . filetype($dir . $file) . "\n";
-        }
-        closedir($dh);
+  if ($dh = opendir($dir)) {
+    while (($file = readdir($dh)) !== false) {
+      echo $file . "<br/>";
     }
+    closedir($dh);
+  }
 }
 
+?>
--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php include 'header.php' ?>
 
-<!-- <div class="background"> -->
   <div class="container">
-    <!-- <div class="col"> -->
       <div class="page">
-      <?php include 'function.php' ?>
-
+        <?php include 'function.php' ?>
       </div>
-    <!-- </div> -->
   </div>
-<!-- </div> -->
 
 <?php include 'footer.php' ?>
