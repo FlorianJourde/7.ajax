9c1b278f6966c3f6c1af42f2b83cdbd5da9a4855
Florian <jourdeflorian@gmail.com>
2021-03-02 09:37:56 +0100

    file or folder

--- function.php
+++ function.php
@@ -1,11 +1,17 @@
 <?php
 
 $dir = "/var/www/html/7.ajax";
+$dot = ".";
 
 if (is_dir($dir)) {
   if ($dh = opendir($dir)) {
     while (($file = readdir($dh)) !== false) {
-      echo $file . "<br/>";
+      $dotPos = strpos($file, $dot);
+      if ($dotPos !== false && $dotPos > 0) {
+        echo "<span class='file '> $file</span>";
+      } else {
+        echo "<span class='folder '> $file</span>";
+      }
     }
     closedir($dh);
   }
